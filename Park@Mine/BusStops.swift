3a585b6812551ac37f612cdf0d507b01ca4bdfa0
Sammy Yousif <syousif@utexas.edu>
2016-02-29 17:36:26 -0600

    rode the bus and updated the west campus stops for constructions

--- Park@Mine/BusStops.swift
+++ Park@Mine/BusStops.swift
@@ -10,7 +10,7 @@ import Foundation
 
 let wcStops = [
     CLLocationCoordinate2D(latitude: 30.28956, longitude: -97.73663), // DEAN KEETON/SPEEDWAY NE CORNER',
-    CLLocationCoordinate2D(latitude: 30.28954, longitude: -97.74775), // SAN GABRIEL / 25TH',
+    CLLocationCoordinate2D(latitude: 30.290050, longitude: -97.747693), // SAN GABRIEL / 25TH',
     CLLocationCoordinate2D(latitude: 30.2834, longitude: -97.7377), // SPEEDWAY / 21ST',
     CLLocationCoordinate2D(latitude: 30.283679, longitude: -97.74128), // 21ST/WHITIS (DOBIE MALL)',
     CLLocationCoordinate2D(latitude: 30.292, longitude: -97.74022), // 27TH/WHITIS',
@@ -19,10 +19,11 @@ let wcStops = [
     CLLocationCoordinate2D(latitude: 30.290696, longitude: -97.744249), // 26TH/RIO GRANDE',
     CLLocationCoordinate2D(latitude: 30.29086, longitude: -97.746437), // 26TH/PEARL',
     CLLocationCoordinate2D(latitude: 30.28313, longitude: -97.73439), // 309 21ST/SAN JACINTO',
-    CLLocationCoordinate2D(latitude: 30.285874, longitude: -97.747959), // 2212 SAN GABRIEL/22ND HALF',
+    CLLocationCoordinate2D(latitude: 30.286465, longitude: -97.748053), // 2212 SAN GABRIEL/22ND HALF',
     CLLocationCoordinate2D(latitude: 30.28972, longitude: -97.7388), // 116 DEAN KEETON/UNIVERSITY',
     CLLocationCoordinate2D(latitude: 30.291657, longitude: -97.742835), // NUECES/27TH',
-    CLLocationCoordinate2D(latitude: 30.286032, longitude: -97.733433) // SAN JACINTO/23RD MIDBLOCK'
+    CLLocationCoordinate2D(latitude: 30.286032, longitude: -97.733433), // SAN JACINTO/23RD MIDBLOCK'
+    CLLocationCoordinate2D(latitude: 30.283523, longitude: -97.739528) // UNIVERSITY/21ST'
 ]
 
 let stopAnnotations = wcStops.map { (location: CLLocationCoordinate2D) -> SpotAnnotation in
